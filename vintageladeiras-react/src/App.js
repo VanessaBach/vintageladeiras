f603b8d61f266e61d88c2c601e740b1c078ce11d
VanessaBach <vanessabach.r@gmail.com>
2023-11-08 12:54:18 +0000

    fix

--- vintageladeiras-react/src/App.js
+++ vintageladeiras-react/src/App.js
@@ -1,17 +1,36 @@
 import './App.css';
-
-const fetchTestDatas = function () {
-  fetch('/api/v1/fridges')
-    .then(res => res.json())
-    .then((response) => { console.log("Test datas response", response); })
-    .catch((error) => { console.log("Error while fetching test datas", error); })
-}
+import React from 'react';
 
 function App() {
+    const fetchTestDatas = function () {
+    fetch('/api/v1/fridges')
+      .then(res => res.json())
+      .then((response) => {
+        console.log("Test datas response", response);
+      })
+      .catch((error) => {
+        console.log("Error while fetching test datas", error);
+      })
+  }
+
+  const fetchAnotherEndpoint = function () {
+    fetch('/api/v1/images')
+      .then(res => res.json())
+      .then((response) => {
+        console.log("Outra rota response", response);
+      })
+      .catch((error) => {
+        console.log("Error while fetching outra rota", error);
+      });
+  }
+
   return (
     <div className="App">
       <button onClick={fetchTestDatas}>
         Fetch Test Dat
+      </button>
+      <button onClick={fetchAnotherEndpoint}>
+        Fetch Another Endpoint
       </button>
     </div>
   );
